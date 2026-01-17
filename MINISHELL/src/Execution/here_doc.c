e99d8ee4f8adcac844112ab6df37a565b417fc8a
Jean Michel Eid <jeid@c1r3s7.42beirut.com>
2026-01-17 17:33:39 +0200

    done

--- MINISHELL/src/Execution/here_doc.c
+++ MINISHELL/src/Execution/here_doc.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jm_eid <jm_eid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jeid <jeid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 21:30:07 by jeid              #+#    #+#             */
-/*   Updated: 2026/01/16 10:00:32 by jm_eid           ###   ########.fr       */
+/*   Updated: 2026/01/17 17:24:42 by jeid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void sigint_handler(int sig)
+void	sigint_handler(int sig)
 {
 	(void)sig;
 	write(STDOUT_FILENO, "\n", 1);
@@ -23,32 +23,32 @@ void sigint_handler(int sig)
 	exit(0);
 }
 
-void signal_heredoc(void)
+void	signal_heredoc(void)
 {
 	signal(SIGINT, sigint_handler);
 	signal(SIGQUIT, SIG_IGN);
 }
 
-char **inside_while(t_env **env, char *eof, char **input_line, int fd)
+char	**inside_while(t_env **env, char *eof, char **input_line, int fd)
 {
 	while (1)
 	{
 		signal_heredoc();
 		(*env)->here_doc = TRUE;
 		if (!eof || g_signal == 130)
-			break;
+			break ;
 		input_line[0] = readline(">");
 		if (input_line[0] == NULL || g_signal == 130)
 		{
 			free(input_line[0]);
-			break;
+			break ;
 		}
 		if ((*env)->quote_indentifier == FALSE)
 			input_line = expansion(*env, input_line);
 		if (is_delimiter(input_line[0], eof))
 		{
 			free(input_line[0]);
-			break;
+			break ;
 		}
 		write(fd, input_line[0], ft_strlen(input_line[0]));
 		write(fd, "\n", 1);
@@ -57,11 +57,11 @@ char **inside_while(t_env **env, char *eof, char **input_line, int fd)
 	return (input_line);
 }
 
-void handle_heredoc(t_env **env, char *eof)
+void	handle_heredoc(t_env **env, char *eof)
 {
-	char **input_line;
-	int fd;
-	int pid;
+	char	**input_line;
+	int		fd;
+	int		pid;
 
 	fd = 0;
 	pid = fork();
